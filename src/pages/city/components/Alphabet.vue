2a8f9cadf8dbedb29277f1d7e12ea468c8085ad4
adar <chiqian.adar@gmail.com>
2020-03-15 18:07:47 +0800

    add aphabet

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,83 @@
+<template>
+  <ul class="list">
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleLetterClick"
+    >{{item}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CityAlphabet",
+  props: {
+    cities: Object
+  },
+  computed: {
+    letters() {
+      const letters = [];
+      for (let i in this.cities) {
+        letters.push(i);
+      }
+      // ['A','B','C']
+      return letters;
+    }
+  },
+  data() {
+    return {
+      touchStatus: false
+    };
+  },
+  methods: {
+    handleLetterClick(e) {
+      this.$emit("change", e.target.innerText);
+    },
+    handleTouchStart() {
+      this.touchStatus = true;
+    },
+    handleTouchMove(e) {
+      if (this.touchStatus) {
+        //A字母距顶部的高度
+        const startY = this.$refs['A'][0].offsetTop;
+        //手指位置高度
+        const touchY = e.touches[0].clientY - 79;
+        //字母位置，20为每个字母高度
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd() {
+      this.touchStatus = false;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/style/varibles.styl';
+
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 1.68rem;
+  right: 0;
+  bottom: 0;
+  width: 0.4rem;
+}
+
+.item {
+  line-height: 0.4rem;
+  text-align: center;
+  color: $bgColor;
+}
+</style>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,80 +1,53 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper" v-for="item in hot" :key="item.id" >
+            <div class="button">{{item.name}}</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area border-topbottom">
-      <div class="title">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area border-topbottom">
-      <div class="title">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area border-topbottom">
-      <div class="title">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area border-topbottom">
-      <div class="title">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
+      <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: "CityList"
-};
+  name: "CityList",
+  props: {
+    hot: Array,
+    cities: Object,
+    letter: String
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if(this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
@@ -92,14 +65,14 @@ export default {
 .list {
   overflow: hidden;
   position: absolute;
-  top: 1.58rem;
+  top: 1.78rem;
   left: 0;
   right: 0;
   bottom: 0;
 }
 
 .title {
-  line-height: 0.44rem;
+  line-height: 0.54rem;
   background: #eee;
   padding-left: 0.2rem;
   color: #666;
